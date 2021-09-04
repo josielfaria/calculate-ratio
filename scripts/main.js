41582e0ec77a9bf3800e7a961393b26cc634ce47
Josiel Faria <developerjosiel@gmail.com>
2021-09-04 13:02:23 -0300

    Adicionando demonstração para vizualizar as proporções

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
-const elemVal1 = document.getElementById("val1");
-const elemVal2 = document.getElementById("val2");
-const elemR1 = document.getElementById("ratio1");
-const elemR2 = document.getElementById("ratio2");
+let elemVal1 = document.getElementById("val1");
+let elemVal2 = document.getElementById("val2");
+let elemR1 = document.getElementById("ratio1");
+let elemR2 = document.getElementById("ratio2");
 const idResult = document.getElementById("idResult");
 const idNoCheck = document.getElementById("idNoCheck");
 const idCheck = document.getElementById("idCheck");
+const idDemoRatio = document.getElementById("idDemoRatio");
 
 const defaultR1 = 8;
 const defaultR2 = 6;
 const defaultVal1 = 800;
 const defaultVal2 = 600;
+const numbDefault = 245;
 
 elemVal1.value = defaultVal1;
 elemVal2.value = defaultVal2;
@@ -26,11 +28,12 @@ function calculateVal1(val) {
   return (val / elemR2.value) * elemR1.value;
 }
 
-function isInteger(val) {
-  return (val % 1 === 0) ? val : val.toFixed(2)
+function convertToPixels(val) {
+  return val + 'px';
 }
 
 function setResult() {
+  calculateFim();
   idResult.innerText = `${elemVal1.value}x${elemVal2.value}`;
 }
 
@@ -53,18 +56,38 @@ function copy() {
   setClipboardDefault();
 }
 
-
 function initEventListener() {
+  setResult();
+
   elemVal1.onkeyup = elemR1.onkeyup = elemR2.onkeyup = function () {
-    elemVal2.value = isInteger(calculateVal2(elemVal1.value));
+    elemVal2.value = calculateVal2(elemVal1.value).toFixed(0);
     setResult();
   }
 
   elemVal2.onkeyup = function () {
-    elemVal1.value = isInteger(calculateVal1(elemVal2.value));
+    elemVal1.value = calculateVal1(elemVal2.value).toFixed(0);
     setResult();
   }
 }
 
-setResult();
+function calculateFim() {
+  let valorFim;
+  const val1 = parseInt(elemVal1.value);
+  const val2 = parseInt(elemVal2.value);
+
+  if (val1 === val2) {
+    idDemoRatio.style.height = convertToPixels(numbDefault);
+    idDemoRatio.style.width = convertToPixels(numbDefault);
+  } else if (val1 > val2) {
+    valorFim = (val2 * numbDefault) / val1;
+    idDemoRatio.style.width = convertToPixels(numbDefault);
+    idDemoRatio.style.height = convertToPixels(valorFim.toFixed(0));
+  } else if (val2 > val1) {
+    valorFim = (val1 * numbDefault) / val2;
+    idDemoRatio.style.width = convertToPixels(valorFim.toFixed(0));
+    idDemoRatio.style.height = convertToPixels(numbDefault);
+
+  }
+}
+
 initEventListener();
